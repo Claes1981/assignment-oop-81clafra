505b6219a3e5512788033de3e7f7205d22d3b1cc
Claes Fransson <81clafra@gafe.molndal.se>
2025-09-18 19:00:43 +0200

    Initial method definitions to remove errors, created with help of Visual Studio Code.

--- OOPAssignment/InventoryManager.cs
+++ OOPAssignment/InventoryManager.cs
@@ -1,11 +1,32 @@
 // See https://aka.ms/new-console-template for more information
 
 // Huvudklassen som hanterar all logik
+
 public class InventoryManager
 {
     private List<Product> products;
     private List<Order> orders;
 
-    // Metoder: LoadProductsFromCsv(), LoadOrdersFromCsv(),
-    //          ProcessOrders(), SaveUpdatedProductsToCsv()
+    internal void LoadProductsFromCsv(string v)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void LoadOrdersFromCsv(string v)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void ProcessOrders()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal void SaveUpdatedProductsToCsv(string v)
+    {
+        throw new NotImplementedException();
+    }
+
+
+
 }
