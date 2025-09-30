Claes1981/assignment-oop-81clafra

commit 42f787bdd3234edfd6503d3deca9291e1d9250d5

Fix forgotten magic numbers.

OOPAssignment/InventoryManager.cs
@@ -89,7 +89,8 @@ public class InventoryManager
                 orders.Add(order);
             }
             else
-                Console.WriteLine($"Fel format i datafil på antal för order från {orderDataElementsArray[1]}, {orderDataElementsArray[2]}.");
+                Console.WriteLine($"Fel format i datafil på antal för order från {orderDataElementsArray[                    CustomerName = orderDataElementsArray[ORDER_CUSTOMER_NAME_INDEX],
+]}, {orderDataElementsArray[ORDER_PRODUCT_NAME_INDEX]}.");
 
         }
         Console.WriteLine();

commit b26f1d56f478f1557353549cab6032daea091744

Fix syntax error accidentally introduced in commit 42f787bdd3234edfd6503d3deca9291e1d9250d5

OOPAssignment/InventoryManager.cs
@@ -89,8 +89,7 @@ public class InventoryManager
                 orders.Add(order);
             }
             else
-                Console.WriteLine($"Fel format i datafil på antal för order från {orderDataElementsArray[                    CustomerName = orderDataElementsArray[ORDER_CUSTOMER_NAME_INDEX],
-]}, {orderDataElementsArray[ORDER_PRODUCT_NAME_INDEX]}.");
+                Console.WriteLine($"Fel format i datafil på antal för order från {orderDataElementsArray[ORDER_CUSTOMER_NAME_INDEX]}, {orderDataElementsArray[ORDER_PRODUCT_NAME_INDEX]}.");
 
         }
         Console.WriteLine();
@@ -172,6 +171,3 @@ public class InventoryManager
         }
     }
 }
-
-
-